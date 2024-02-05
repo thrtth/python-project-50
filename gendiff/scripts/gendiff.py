5aaa7be3e796c1d237e50cf155ff7fc23719248b
Aleх Bark <thrtth@yandex.ru>
2024-02-05 12:34:41 +0700

    Add badges

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,7 +3,9 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares'
+                                                 ' two configuration files'
+                                                 ' and shows a difference.')
     parser.add_argument('-f', '--format', type=str, help='set format of output')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
