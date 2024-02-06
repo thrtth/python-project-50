78386ead4c483a6124fd2e5521bb973511458aec
Aleх Bark <thrtth@yandex.ru>
2024-02-06 17:33:49 +0700

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,11 +1,19 @@
+import pytest
+
 from gendiff import generate_diff
 
 
-def test_generate_diff():
+@pytest.fixture
+def get_expect_result():
+    with open('tests/fixtures/result.txt') as res_file:
+        return res_file.read()
+
+
+def test_json(get_expect_result):
     result = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
-    assert result == ('{\n  - follow: false\n'
-                      '    host: hexlet.io\n'
-                      '  - proxy: 123.234.53.22\n'
-                      '  - timeout: 50\n'
-                      '  + timeout: 20\n'
-                      '  + verbose: true\n}')
+    assert result == get_expect_result
+
+
+def test_yaml(get_expect_result):
+    result = generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml')
+    assert result == get_expect_result
