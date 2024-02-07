fed43118f0c796ef299e1570181ef4718c62e4d3
Aleх Bark <thrtth@yandex.ru>
2024-02-07 13:51:26 +0700

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff.modules.generate_diff import generate_diff
 
 
 def main():
@@ -9,7 +10,8 @@ def main():
     parser.add_argument('-f', '--format', type=str, help='set format of output')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    # args = parser.parse_args()
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
