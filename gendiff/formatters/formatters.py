59a75673473bbc972c4accd4bff1d8c55b01b092
Aleх Bark <thrtth@yandex.ru>
2024-02-12 17:01:19 +0700

    Add plain formatter

---
+++ gendiff/formatters/formatters.py
@@ -0,0 +1,95 @@
+INDENT = 4
+
+
+def to_stylish(list_of_dicts, depth=0):
+    result = '{\n'
+    for item in list_of_dicts:
+        item_type = item['type']
+        match item_type:
+            case 'unchanged':
+                value = value_to_stylish(item["value"], depth)
+                result += f'{" " * depth}    {item["key"]}: {value}\n'
+
+            case 'changed':
+                old_value = value_to_stylish(item["old_value"], depth)
+                new_value = value_to_stylish(item["new_value"], depth)
+                result += f'{" " * depth}  - {item["key"]}: {old_value}\n'
+                result += f'{" " * depth}  + {item["key"]}: {new_value}\n'
+
+            case 'added':
+                new_value = value_to_stylish(item["new_value"], depth)
+                result += f'{" " * depth}  + {item["key"]}: {new_value}\n'
+
+            case 'removed':
+                old_value = value_to_stylish(item["old_value"], depth)
+                result += f'{" " * depth}  - {item["key"]}: {old_value}\n'
+
+            case 'nested':
+                result += (f'{" " * depth}    {item["key"]}:'
+                           f' {to_stylish(item["value"], depth + INDENT)}\n')
+
+    result += ' ' * depth + '}'
+    return result
+
+
+def to_plain(list_of_dicts, path_to_value=''):
+    result = ''
+    for item in list_of_dicts:
+        item_type = item['type']
+        new_path = (path_to_value + "." + item["key"]).strip('.')
+        match item_type:
+            case 'changed':
+                result += (f"Property '{new_path}' was updated. "
+                           f"From {value_to_plain(item['old_value'])} "
+                           f"to {value_to_plain(item['new_value'])}\n")
+
+            case 'added':
+                result += (f"Property '{new_path}' "
+                           f"was added with value: "
+                           f"{value_to_plain(item['new_value'])}\n")
+
+            case 'removed':
+                result += f"Property '{new_path}' was removed\n"
+
+            case 'nested':
+                result += to_plain(item["value"], new_path)
+    return result
+
+
+def dict_to_str(some_dict, depth):
+    result = '{\n'
+    dict_keys = some_dict.keys()
+    for key in dict_keys:
+        if isinstance(some_dict[key], dict):
+            result += (f'{" " * depth}    {key}:'
+                       f' {dict_to_str(some_dict[key], depth + INDENT)}\n')
+        else:
+            value = value_to_stylish(some_dict[key], depth)
+            result += f'{" " * depth}    {key}: {value}\n'
+    result += ' ' * depth + '}'
+    return result
+
+
+def value_to_stylish(value, depth):
+    if isinstance(value, dict):
+        return dict_to_str(value, depth + INDENT)
+    return bool_and_none_to_str(value)
+
+
+def value_to_plain(value):
+    if isinstance(value, dict):
+        return '[complex value]'
+    elif isinstance(value, bool) or value is None:
+        return bool_and_none_to_str(value)
+    else:
+        return f"'{value}'"
+
+
+def bool_and_none_to_str(val):
+    if val is False:
+        return 'false'
+    elif val is True:
+        return 'true'
+    elif val is None:
+        return 'null'
+    return val
--- gendiff/modules/generate_diff.py
+++ gendiff/modules/generate_diff.py
@@ -1,19 +1,8 @@
 import json
 from os import path
 import yaml
-
-
-INDENT = 4
-
-
-def bool_and_none_to_str(val):
-    if val is False:
-        return 'false'
-    elif val is True:
-        return 'true'
-    elif val is None:
-        return 'null'
-    return val
+from gendiff.formatters.formatters import to_stylish
+from gendiff.formatters.formatters import to_plain
 
 
 def parse_file(file_path):
@@ -26,15 +15,18 @@ def parse_file(file_path):
     return file_data
 
 
-def generate_diff(file_path1, file_path2, output_format):
-    formater = globals()[output_format]
+def generate_diff(file_path1, file_path2, format_name):
+    formatter = get_formatter(format_name)
     file_data1 = parse_file(file_path1)
     file_data2 = parse_file(file_path2)
-    return formater(get_diff(file_data1, file_data2))
+    return formatter(get_diff(file_data1, file_data2)).strip('\n')
 
 
-def is_dict(node):
-    return type(node) is dict
+def get_formatter(format_name):
+    formatter = to_stylish
+    if format_name == 'plain':
+        formatter = to_plain
+    return formatter
 
 
 def get_diff(dict1, dict2):
@@ -53,7 +45,7 @@ def get_diff(dict1, dict2):
                            'type': 'removed',
                            'old_value': value1})
 
-        elif is_dict(value1) and is_dict(value2):
+        elif isinstance(value1, dict) and isinstance(value2, dict):
             children = get_diff(value1, value2)
             result.append({'key': key,
                            'type': 'nested',
@@ -71,54 +63,3 @@ def get_diff(dict1, dict2):
                            'new_value': value2})
 
     return result
-
-
-def stylish(list_of_dicts, depth=0):
-    result = '{\n'
-    for item in list_of_dicts:
-        item_type = item['type']
-        match item_type:
-            case 'unchanged':
-                value = value_to_str(item["value"], depth)
-                result += f'{" " * depth}    {item["key"]}: {value}\n'
-
-            case 'changed':
-                old_value = value_to_str(item["old_value"], depth)
-                new_value = value_to_str(item["new_value"], depth)
-                result += f'{" " * depth}  - {item["key"]}: {old_value}\n'
-                result += f'{" " * depth}  + {item["key"]}: {new_value}\n'
-
-            case 'added':
-                new_value = value_to_str(item["new_value"], depth)
-                result += f'{" " * depth}  + {item["key"]}: {new_value}\n'
-
-            case 'removed':
-                old_value = value_to_str(item["old_value"], depth)
-                result += f'{" " * depth}  - {item["key"]}: {old_value}\n'
-
-            case 'nested':
-                result += (f'{" " * depth}    {item["key"]}:'
-                           f' {stylish(item["value"], depth + INDENT)}\n')
-
-    result += ' ' * depth + '}'
-    return result
-
-
-def dict_to_str(some_dict, depth):
-    result = '{\n'
-    dict_keys = some_dict.keys()
-    for key in dict_keys:
-        if is_dict(some_dict[key]):
-            result += (f'{" " * depth}    {key}:'
-                       f' {dict_to_str(some_dict[key], depth + INDENT)}\n')
-        else:
-            value = value_to_str(some_dict[key], depth)
-            result += f'{" " * depth}    {key}: {value}\n'
-    result += ' ' * depth + '}'
-    return result
-
-
-def value_to_str(value, depth):
-    if is_dict(value):
-        return dict_to_str(value, depth + INDENT)
-    return bool_and_none_to_str(value)
