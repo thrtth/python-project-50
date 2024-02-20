44a2034ce38e70793cdb39816d830eca2223f649
Aleх Bark <thrtth@yandex.ru>
2024-02-20 13:06:20 +0700

    Refactoring main script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
-import argparse
+from gendiff.modules.arg_parser import get_args
 from gendiff.modules.generate_diff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares'
-                                                 ' two configuration files'
-                                                 ' and shows a difference.')
-    parser.add_argument('-f', '--format',
-                        type=str,
-                        default='stylish',
-                        help='set format of output (default: "stylish")')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    args = parser.parse_args()
+    args = get_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
 
