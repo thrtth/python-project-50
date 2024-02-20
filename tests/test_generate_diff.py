638f223abceea3e5f740720b1f2519cb81f20079
Aleх Bark <thrtth@yandex.ru>
2024-02-20 13:04:59 +0700

    Refactoring tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,25 +3,27 @@ import pytest
 from gendiff import generate_diff
 
 
-def get_absolute_path(relative_path):
-    return os.path.abspath(relative_path)
+def get_fixtures_path():
+    return os.path.join(os.path.dirname(__file__), 'fixtures')
 
 
 @pytest.mark.parametrize("file1, file2, format_name, expected",
-                         [("tests/fixtures/file1.json", "tests/fixtures/file2.json",
-                           "stylish", "tests/fixtures/result_stylish.txt"),
-                          ("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml",
-                           "stylish", "tests/fixtures/result_stylish.txt"),
-                          ("tests/fixtures/file1.json", "tests/fixtures/file2.json",
-                           "plain", "tests/fixtures/result_plain.txt"),
-                          ("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml",
-                           "plain", "tests/fixtures/result_plain.txt"),
-                          ("tests/fixtures/file1.json", "tests/fixtures/file2.json",
-                           "json", "tests/fixtures/result_json.txt"),
-                          ("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml",
-                           "json", "tests/fixtures/result_json.txt")])
+                         [("file1.json", "file2.json",
+                           "stylish", "result_stylish.txt"),
+                          ("file1.yaml", "file2.yaml",
+                           "stylish", "result_stylish.txt"),
+                          ("file1.json", "file2.json",
+                           "plain", "result_plain.txt"),
+                          ("file1.yaml", "file2.yaml",
+                           "plain", "result_plain.txt"),
+                          ("file1.json", "file2.json",
+                           "json", "result_json.txt"),
+                          ("file1.yaml", "file2.yaml",
+                           "json", "result_json.txt")])
 def test_generate_diff(file1, file2, format_name, expected):
-    with open(get_absolute_path(expected)) as res_file:
+    with open(os.path.join(get_fixtures_path(), expected)) as res_file:
         expected_result = res_file.read()
-    result = generate_diff(get_absolute_path(file1), get_absolute_path(file2), format_name)
+    result = generate_diff(os.path.join(get_fixtures_path(), file1),
+                           os.path.join(get_fixtures_path(), file2),
+                           format_name)
     assert result == expected_result
